6f24efe5c62a7be863e122a50567c84d36ae932c
Alexandr <wizzardnarhiz@yandex.ru>
2024-04-14 15:49:53 +0200

    second commit

---
+++ 9. Course work #1/Assets/PixelCrew/Components/MobilePlatform.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class MobilePlatform : MonoBehaviour
+    {
+        [SerializeField] private int _speed;
+
+        private Rigidbody2D _rigidbody;
+        private int _direction;
+        public int xVelocity;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (transform.position.x <= -12)
+            {
+                _direction = 1;
+            }
+            if (transform.position.x >= -5)
+            {
+                _direction = -1;
+            }
+            xVelocity = _direction * _speed;
+			//var yVelocity = _direction * _speed;
+			_rigidbody.velocity = new Vector2(xVelocity, 0);
+		}
+    }
+}
+
